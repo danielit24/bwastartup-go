5e4e65c3ba9e2cbcb433845359d93a93ec0c68a3
Daniel <daniel.ecc@ruparupa.com>
2023-02-10 22:31:52 +0700

    first commit

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/danielit24/bwastartup
+
+go 1.16
+
+require (
+	github.com/gin-gonic/gin v1.8.2
+	github.com/go-playground/validator/v10 v10.11.1
+	golang.org/x/crypto v0.0.0-20211215153901-e495a2d5b3d3
+	gorm.io/driver/mysql v1.4.5
+	gorm.io/gorm v1.24.5
+)
---
+++ user/formatter.go
@@ -0,0 +1,20 @@
+package user
+
+type UserFormatter struct {
+	Id         int    `json:"id"`
+	Name       string `json:"name"`
+	Occupation string `json:"occupation"`
+	Email      string `json:"email"`
+	Token      string `json:"token"`
+}
+
+func FormatUser(user User, token string) UserFormatter {
+	formatter := UserFormatter{
+		Id:         user.ID,
+		Name:       user.Name,
+		Email:      user.Email,
+		Occupation: user.Occupation,
+		Token:      token,
+	}
+	return formatter
+}
